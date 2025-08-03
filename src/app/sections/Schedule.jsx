3d46c8a9f55cfe7423899c87f40fbe6817d0bdcb
Chen Zhutian <zhutian.chen.code@outlook.com>
2025-08-03 18:23:58 -0500

    fix: init schedule

--- src/app/sections/Schedule.jsx
+++ src/app/sections/Schedule.jsx
@@ -4,11 +4,62 @@ import React from "react";
 
 export default function Schedule() {
     return (
-    <section id="schedule" className="section">
+      <section id="schedule" className="section">
       <h2 className="text-3xl font-bold text-gray-800 mb-4">Schedule</h2>
-        <p className="text-gray-700 mb-4 italic font-semibold">TBA soon! </p>
+      <p className="text-gray-700 mb-4 italic font-semibold">Details TBA soon! </p>
+      <div className="overflow-x-auto mb-10 rounded-lg">
+        <table
+        className="border border-gray-300"
+        style={{ width: "750px" }}
+        >
+        <colgroup>
+          <col style={{ width: "220px" }} />
+          <col />
+        </colgroup>
+        <tbody className="divide-y divide-gray-200">
+          <tr className="bg-gray-100">
+          <td className="px-4 py-2 align-top font-bold">
+            Vienna Time (UTC+2)
+            <br />
+            {/* <span className="text-sm text-gray-600">(10 min)</span> */}
+          </td>
+          <td className="px-4 py-2 font-bold">Events</td>
+          </tr>
+          <tr>
+          <td className="px-4 py-2 align-top">
+            9:00 am - 12:30pm
+            <br />
+            {/* <span className="text-sm text-gray-600">(10 min)</span> */}
+          </td>
+          <td className="px-4 py-2">
+            Morning Session
+          </td>
+          </tr>
+          <tr>
+          <td className="px-4 py-2 align-top">
+            12:30 pm – 2:00 pm
+            <br />
+            {/* <span className="text-sm text-gray-600">(50 min)</span> */}
+          </td>
+          <td className="px-4 py-2">
+            Lunch Break
+          </td>
+          </tr>
+          <tr>
+          <td className="px-4 py-2 align-top">
+            2:00 pm - 5:30 pm
+            <br />
+            {/* <span className="text-sm text-gray-600">(30 min)</span> */}
+          </td>
+          <td className="px-4 py-2">
+            Afternoon Session
+          </td>
+          </tr>
+        </tbody>
+        </table>
+      </div>
       </section>
-  )
+    )
 }
 
 export function FullSchedule() {
@@ -130,22 +181,7 @@ export function FullSchedule() {
               <td className="px-4 py-2">Workshop summary</td>
             </tr>
           </tbody>
-          {/* <tfoot>
-            <tr>
-              <th
-                colSpan={2}
-                className="px-4 py-2 border-t border-gray-300 bg-gray-100 italic text-center"
-              >
-                After the workshop
-              </th>
-            </tr>
-            <tr>
-              <td className="px-4 py-2 align-top">within one week</td>
-              <td className="px-4 py-2">
-                Posting summary and records on website. Initiating follow-up activities.
-              </td>
-            </tr>
-          </tfoot> */}
+
         </table>
       </div>
     </section>
